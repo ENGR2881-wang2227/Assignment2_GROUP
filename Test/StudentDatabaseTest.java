b910b6a07f5133b042b64c1a3f6e1fa737eb48be
Justin-Control-Case <58718370+Justin-Control-Case@users.noreply.github.com>
2023-06-01 13:33:14 +0930

    added test files (unfilled)

---
+++ Test/StudentDatabaseTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentDatabaseTest {
+
+    @Test
+    void getDb() {
+    }
+
+    @Test
+    void addStudent() {
+    }
+
+    @Test
+    void findStudent() {
+    }
+
+    @Test
+    void addResult() {
+    }
+
+    @Test
+    void findResults() {
+    }
+
+    @Test
+    void addRewards() {
+    }
+
+    @Test
+    void printRecords() {
+    }
+
+    @Test
+    void clearRecords() {
+    }
+
+    @Test
+    void readFile() {
+    }
+
+    @Test
+    void recordsToFile() {
+    }
+}
---
+++ Test/StudentTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    void setDegree() {
+    }
+
+    @Test
+    void getLastName() {
+    }
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void getStudentID() {
+    }
+
+    @Test
+    void getDegree() {
+    }
+
+    @Test
+    void getTopicCount() {
+    }
+
+    @Test
+    void getResult() {
+    }
+
+    @Test
+    void addResult() {
+    }
+
+    @Test
+    void resultSize() {
+    }
+
+    @Test
+    void show() {
+    }
+}
